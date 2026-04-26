aecb8bedc551a43d173b005ff6ef3d6cfc66ed4f
Dennis Wu <67745938+Hibb-bb@users.noreply.github.com>
2026-04-26 15:59:59 -0500

    Update pred.sh

--- pred.sh
+++ pred.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-#SBATCH --account=p32593
-#SBATCH --job-name=pred_jasa
-#SBATCH --nodes=1
-#SBATCH --partition=gengpu
-#SBATCH --gres=gpu:a100:1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=40G
-#SBATCH --time=24:00:00
-#SBATCH --output=pred.out
-#SBATCH --error=pred.err
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=hibb@u.northwestern.edu
-
-
 source .venv/bin/activate
 
 python train_pred.py --graph tree --context-len 500 --num-layers 4 --train-size 1000
